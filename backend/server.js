bd6aa41202226f3663d92e0f8c5f159246c0def9
Hafiz Tanzeel Shamshad <hafiztanzeel@gmail.com>
2025-05-13 21:37:38 +0500

    change port info

--- backend/server.js
+++ backend/server.js
@@ -303,5 +303,5 @@ app.use((err, req, res, next) => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
